8fa942abd4bf95558acafbf0f7977369627c97eb
My Chau <nguyenlemychau1201@gmail.com>
2023-11-10 22:33:35 +0700

    Create models, connect database

---
+++ src/main/java/vn/edu/iuh/fit/week05_nguyenlemychau_20046631/backend/enums/SkillType.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.week05_nguyenlemychau_20046631.backend.enums;
+
+public enum SkillType {
+    UNSPECIFIC(0),
+
+    TECHNICAL_SKILL(1),
+
+    SOFT_SKILL(2);
+
+    SkillType(int value) {
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/week05_nguyenlemychau_20046631/Week05NguyenLeMyChau20046631ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.week05_nguyenlemychau_20046631;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week05NguyenLeMyChau20046631ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
